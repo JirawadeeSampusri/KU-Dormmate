1ed10819ea66e2b15885cf1ad3ba58e218ad8171
JirawadeeSampusri <jirawadeesampu@gmail.com>
2022-01-19 00:29:46 +0700

    navbar edit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+      <div className="App bg-[url('./assets/bg-presentation.jpg')] ">
       <Navbar />
       <div className="content">
         <Switch>
--- src/navbar.js
+++ src/navbar.js
@@ -1,57 +1,164 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import MenuIcon from './assets/bars-solid.svg'
-import CloseIcon from './assets/window-close-regular.svg'
-<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css" rel="stylesheet" />
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
+const navigation = [
+  { name: 'Home', href: '', current: false },
+  { name: 'Setting', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+  { name: 'Add Blog', href: './create', current: false },
+]
 
-
-const Navbar = () => {
-
-    const [isSideMenuOpen, setisSideMenuOpen] = useState(false)
-    const showSideMenu = ()=>{
-        (isSideMenuOpen ? setisSideMenuOpen(false) : setisSideMenuOpen(true))
-    }
-
-    return ( 
-        <nav className="fixed w-full h-8 bg-blue-400 text-gray-200 flex flex-row justify-between">
-            <div className=" brand-logo text-sm font-bold">
-                <h1>KU Dorm-Mate</h1> 
-            </div>
-            <div className=" lg:hidden  menu-list flex flex-row  text-xs font-bold">
-                <Link className="menu-list-item px-2" to="/">home</Link>
-                <Link className=" menu-list-item px-2" to="/">Profile</Link>
-                <Link className="menu-list-item px-2" to="/create" style={{
-                    color:"white",
-                    backgroundColor:'#f1356c',
-                    borderRadius: '7px'
-                }}
-                >new blog</Link>       
-            </div>
-
-            <button onClick={()=> {showSideMenu()}} className="menu-button">
-                {(isSideMenuOpen) ? <img src={CloseIcon} className="w-8 h-8 px-2" alt="close"></img> :  <img src={MenuIcon} className="w-8 h-8 px-2" alt="menubar"></img>}
-            </button>
-            {(isSideMenuOpen) ?  SideMenu() : ''}
-           
-        </nav>
-     );
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
-export default Navbar;
 
-function SideMenu(){
-    return(
-        <div className=" fixed h-screen w-1/2 sm:w-1/4  bg-pink-500 top-8">
-            <div className="menu-list flex flex-col text-xs font-bold">
-                <Link className="menu-list-item py-2" bg-white text-blue-700 to="/">home</Link>
-                <Link className="menu-list-item  py-2" bg-white text-blue-700 to="/">Profile</Link>
-                <Link className="menu-list-item py-2" bg-white text-blue-700  to="/create" style={{
-                    color:"white",
-                    backgroundColor:'#f1356c',
-                    borderRadius: '7px'
-                }}
-                >new blog</Link>       
+export default function Example() {
+  return (
+    <Disclosure as="nav" >
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center block laptop:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="absolute inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <img
+                    className="hidden tablet:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  />
+              </div>
+            
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block laptop:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  {/* <img
+                    className="hidden tablet:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  /> */}
+                </div>
+                <div className="hidden tablet:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-teal-600 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  <div>
+                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
             </div>
-        </div>
-    )
+          </div>
+
+          <Disclosure.Panel className="block labtop:hidden">
+            <div className="bg-teal-600 px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
 }
